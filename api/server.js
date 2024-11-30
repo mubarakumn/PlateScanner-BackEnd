dd341e6755d528e9647f33ad06f55924e4e27439
mubarak usman <mubarak725908@gmail.com>
2024-11-30 01:49:00 +0100

    mongo changes

--- api/server.js
+++ api/server.js
@@ -34,7 +34,11 @@ app.use('/plate', PlateRoutes)
 
 
 // mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/PlateScanner')
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000
+})
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
