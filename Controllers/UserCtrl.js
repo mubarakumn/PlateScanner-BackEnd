8cf2cfb4a9be8dd1e324424e502db8f1e78295e5
mubarak usman <mubarak725908@gmail.com>
2025-02-15 14:32:00 +0100

    remove some console logs

--- Controllers/UserCtrl.js
+++ Controllers/UserCtrl.js
@@ -154,10 +154,10 @@ const resetPassword = async (req, res) => {
 // checkSubscription
 const checkSubscription = async (req, res) => {
   const  email = req.query.email;
-  console.log(email);   
+  // console.log(email);   
   try {
     const user = await UserModel.findOne({ email });
-    console.log(user);
+    // console.log(user);
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
